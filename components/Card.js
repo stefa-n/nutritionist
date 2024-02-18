3edfc00dc1fe63243b6ff1d21126a81b68861d0f
stefa-n <67799417+stefa-n@users.noreply.github.com>
2024-02-18 21:56:37 +0200

    update chestii trestii

--- components/Card.js
+++ components/Card.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 import styles from "./styles/Card.module.css";
 import Tooltip from "./Card/Tooltip";
 
-export default function Card({ name, image, calories, nutriscore, novascore }) {
+export default function Card({ brand, name, image, calories, nutriscore, novascore }) {
     return (
         <div className={styles.wrapper}>
             <Image src={image} alt={name} width={200} height={200} className={styles.image}/>
             <div className={styles.textArea}>
-                <p className={styles.title}>{name}</p>
+                <p className={styles.title}>{brand} - {name}</p>
                 <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center', position: 'absolute', bottom: '8px', width: '100%'}}>
                     <Tooltip type="cals" text={calories + "kcal"} color="#C3E8A6"/>
                     <Tooltip type="nutri" text={"NU:" + nutriscore} color="#D0576D"/>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ import styles from "@/styles/Home.module.css";
 import Topbar from "@/components/Topbar";
 import Card from "@/components/Card";
 
-export default function Home() {
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient('https://devjuheafwjammjnxthd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRldmp1aGVhZndqYW1tam54dGhkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwODI4MTg5MiwiZXhwIjoyMDIzODU3ODkyfQ.RHiqiCEAMLAoJVJ-F07Hcby3MmjR5HpC_su0DbDsFS4')
+
+export default function Home({ products }) {
   return (
     <>
       <Head>
@@ -15,20 +18,35 @@ export default function Home() {
       <main className={`${styles.main}`}>
         <Topbar />
           <div className={`${styles.grid}`}>
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
-          <Card name="VIVA - Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
+          <Card brand="VIVA" name="Pernuțe umplute cu cremă de cacao" image="https://shopius.ro/wp-content/uploads/2020/11/sc3f9rs_viva_pernite_cacao_100g_1.png" calories={70} nutriscore="E" novascore={2} />
+          {products.map((product) => (
+            <Card brand={product.brand} name={product.product_name} image={product.image} calories={product.kcal} nutriscore={0} novascore={0} />
+          ))}
         </div>
       </main>
     </>
   );
+}
+export async function getServerSideProps() {
+  const { data, error } = await supabase
+    .from('products')
+    .select('*')
+
+  const produse = data;
+
+  for (let i = 0; i < data.length; i++) {
+    const { data } = supabase
+      .storage
+      .from('products')
+      .getPublicUrl('5449000306821.jpg')
+
+    console.log(data.publicUrl)
+    produse[i].image = `${data.publicUrl}`
+  }
+
+  return {
+    props: {
+      products: produse
+    }
+  }
 }
