9e46c69cf1797611a0ef8f41d6b594b345521013
radupasparuga <radupasparuga25@gmail.com>
2024-03-20 21:08:14 +0200

    fix

--- pages/api/admin/supabase.js
+++ pages/api/admin/supabase.js
@@ -1,4 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
 export const admin_supabase = createClient(
-    "https://devjuheafwjammjnxthd.supabase.co",
-    "FyTbRMiB+0EXrFDkxNxdSvE75x42pMEHJ6Xp4x6P1cp+BRUXhdMu//LjH0VmTraVSommII9S+JxJ8zrQS0oVwA=="
+  "https://devjuheafwjammjnxthd.supabase.co",
+  "FyTbRMiB+0EXrFDkxNxdSvE75x42pMEHJ6Xp4x6P1cp+BRUXhdMu//LjH0VmTraVSommII9S+JxJ8zrQS0oVwA=="
 );
