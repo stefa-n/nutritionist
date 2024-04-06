aff562f8f5cb847111448ec9f4e485df9d071231
stefa-n <67799417+stefa-n@users.noreply.github.com>
2024-04-06 23:16:58 +0300

    fix: cors x2

--- backend/index.js
+++ backend/index.js
@@ -178,17 +178,30 @@ app.get("/communitychallenge", async (req, res) => {
 });
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://fii.stefa-n.xyz http://localhost:3000"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, authorization"
+    "X-Requested-With,content-type, Authorization"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
 
+  next();
+});
+
+app.all("*", function (req, res, next) {
+  const origin = req.headers.origin;
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
